cb21004b96b036d95350bc84f959132994364523
Daniel Guzman <dguzman@creasys.cl>
2022-03-16 17:06:05 -0300

    pruebas unitarias iniciales

---
+++ src/test/java/com/devops/dxc/devops/DxcTest.java
@@ -0,0 +1,34 @@
+package com.devops.dxc.devops;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.devops.dxc.devops.model.Dxc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class DxcTest {
+   @Test
+   void testAhorro() throws Exception {
+         Dxc di = new Dxc(4350000,0);
+         assertEquals(4350000,di.getAhorro() );
+   }
+   
+   @Test
+   void testDiezxciento() throws Exception {
+         Dxc di = new Dxc(23000000,900000);
+         assertEquals(2300000,di.getDxc() );
+   }
+   
+   @Test
+   void testSaldo() throws Exception {
+        Dxc di = new Dxc(100000000,0);
+        assertEquals(95650000,di.getSaldo() );
+   }
+   
+   @Test
+   void testImpuesto()throws Exception   {
+        Dxc di = new Dxc(1000,0);
+        assertEquals(0,di.getImpuesto() );
+   }
+}
